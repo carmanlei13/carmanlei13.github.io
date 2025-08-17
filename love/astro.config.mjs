a98d33418abd6b9a5807e61f47b0895c54206064
upleong <pio.leong@gmail.com>
2025-08-17 11:13:46 +0800

    remove css

--- love/astro.config.mjs
+++ love/astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mithril from '@pioleong/astro-mithril'
-import tailwindcss from '@tailwindcss/vite'
 
 
 // https://astro.build/config
@@ -9,7 +8,6 @@ export default defineConfig({
   // Enable Mithril to support Mithril JSX components.
   integrations: [mithril()],
   vite: {
-    plugins: [tailwindcss()],
     esbuild: {
       jsx: "transform",
       jsxFactory: "m",
